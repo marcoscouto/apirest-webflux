52152053350bb1cc999f98156d14e7b4280fbb59
Marcos Couto <marcos_otuoc@hotmail.com>
2019-12-28 16:44:31 -0300

    Creating document and repository for Playlist entity

---
+++ src/main/java/com/apirest/webflux/repository/PlaylistRepository.java
@@ -0,0 +1,6 @@
+package com.apirest.webflux.repository;
+
+import com.apirest.webflux.document.Playlist;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {}
