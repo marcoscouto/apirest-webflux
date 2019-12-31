d02af0fd3f51abfb5a7493f2d0336cc5ac87a550
Marcos Couto <marcos_otuoc@hotmail.com>
2019-12-31 00:00:27 -0300

    Creating controller class for Playlist entity

---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,31 @@
+package com.apirest.webflux.controller;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+    @Autowired
+    PlaylistService playlistService;
+
+    @GetMapping(value = "/playlist")
+    public Flux<Playlist> getPlaylist(){
+        return playlistService.findAll();
+    }
+
+    @GetMapping(value = "/playlist/{id}")
+    public Mono<Playlist> getPlaylistId(@PathVariable String id){
+        return playlistService.findById(id);
+    }
+
+    @PostMapping(value = "/playlist")
+    public Mono<Playlist> savePlaylist(@RequestBody Playlist playlist){
+        return playlistService.save(playlist);
+    }
+
+}
